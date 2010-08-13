307f1aff805b54cff901f9a699712653c776c6c6
Jasmine Sandhu <sandhu.jasmine@gmail.com>
2010-08-13 12:44:16 -0700

    Moved HelloController.java to controller/.

---
+++ src/main/java/org/springapp/controller/HelloController.java
@@ -0,0 +1,21 @@
+package org.springapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello.ftl")
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        return new ModelAndView("hello");   // no Map associated with ModelAndView
+    }
+}
